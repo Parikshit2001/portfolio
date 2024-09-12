282645652627be4a4ea442bcbb337b3f00f6b2ac
Parikshit <parikshitgupta2001@gmail.com>
2024-09-12 12:54:20 +0530

    Add more projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,60 @@ export default function Home() {
   ];
   const projects: projectInferface[] = [
     {
+      title: "Chat Application",
+      description:
+        "A chat application made using nodeJs, React and drizzle ORM. Using websocket for realtime communication",
+      link: "https://chat-app-psi-neon.vercel.app/",
+    },
+    {
       title: "CDEX - Centralized decentralized exchanage",
       description: "Wallet as a Service - Manage your crypto wallet with ease",
       link: "https://cdex-zeta.vercel.app/",
     },
     {
-      title: "Chat-app",
-      description: "Chat app with drizzle, postgresql, nodejs and React",
-      link: "https://chat-app-psi-neon.vercel.app/",
+      title: "medium",
+      description:
+        "A medium clone made with nextjs and tailwindcss with serverles backend deployed on cloudflare",
+      link: "https://medium-fontend.vercel.app/",
+    },
+    {
+      title: "fireBlog",
+      description:
+        "Blogging website which let you publish and read blogs anonymously. Made with react, tailwindcss and used appwrite Baas",
+      link: "https://fire-blog-abc.vercel.app/",
+    },
+  ];
+
+  const smallProjects: projectInferface[] = [
+    {
+      title: "Travel Buddy",
+      description: "",
+      link: "https://parikshit2001.github.io/travel-website/",
+    },
+    {
+      title: "landing page",
+      description: "",
+      link: "https://parikshit2001.github.io/landing-page/",
+    },
+    {
+      title: "Course explorer",
+      description: "",
+      link: "https://cohort-web3.vercel.app/",
+    },
+    {
+      title: "Paytm clone",
+      description: "",
+      link: "https://paytm-frontend-omega.vercel.app/signin",
+    },
+    {
+      title: "Parallax Scrolling",
+      description: "",
+      link: "https://parikshit2001.github.io/parallax-scrolling/",
+    },
+    {
+      title: "quiz app",
+      description: "",
+      link: "https://quiz-app-eta-eosin.vercel.app/",
     },
   ];
 
@@ -37,7 +83,11 @@ export default function Home() {
       <div className="flex pb-5">
         {socials.map((social, index) => (
           <div className="flex" key={index}>
-            <Link href={social.link} target="_blank" className="underline">
+            <Link
+              href={social.link}
+              target={`${social.link === "" ? "" : "_blank"}`}
+              className="underline"
+            >
               {social.title}
             </Link>
             {index !== socials.length - 1 ? (
@@ -46,17 +96,40 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div>
-        <h1 className="text-4xl py-8">projects</h1>
-        <div className="flex flex-col space-y-8">
-          {projects.map((project) => (
-            <div key={project.title} className="flex flex-col space-y-1">
-              <Link href={project.link} target="_blank" className="underline font-semibold text-lg">
-                {project.title}
-              </Link>
-              <p className="max-w-96">{project.description}</p>
-            </div>
-          ))}
+      <div className="flex space-x-10">
+        <div>
+          <h1 className="text-4xl py-8">projects</h1>
+          <div className="flex flex-col space-y-8">
+            {projects.map((project) => (
+              <div key={project.title} className="flex flex-col space-y-1">
+                <Link
+                  href={project.link}
+                  target="_blank"
+                  className="underline font-semibold text-lg"
+                >
+                  {project.title}
+                </Link>
+                <p className="max-w-96">{project.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl py-8">small frontend projects</h1>
+          <div className="flex flex-col space-y-4">
+            {smallProjects.map((project) => (
+              <div key={project.title} className="flex flex-col space-y-1">
+                <Link
+                  href={project.link}
+                  target="_blank"
+                  className="underline font-semibold "
+                >
+                  {project.title}
+                </Link>
+                <p className="max-w-96">{project.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
